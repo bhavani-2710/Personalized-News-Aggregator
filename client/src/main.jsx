ebfece96d34a3cd71cf6da207615a4300f7ff5bf
Leisha Suvarna <leishasuv29@gmail.com>
2025-02-07 15:33:17 +0530

    created landing page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Layout from "./Layout.jsx";
-import Home from "./pages/Home.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -14,11 +12,12 @@ import Source from "./pages/Source.jsx";
 import LatestNews from "./pages/LatestNews.jsx";
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Landing from "./pages/Landing.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
+    <Route path="/" >
+      <Route path="" element={<Landing />} />
       <Route path="source" element={<Source />} />
       <Route path="latest-news" element={<LatestNews />} />
       <Route path="/login" element={<Login />} />
