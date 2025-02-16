d7e947be6920e90f3f5dcc4100e16a5313033eb1
Bhavani Murali <bhavani9271@gmail.com>
2025-02-16 12:17:47 +0530

    Login page integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -10,18 +9,20 @@ import {
 } from "react-router-dom";
 import Source from "./pages/Source.jsx";
 import LatestNews from "./pages/LatestNews.jsx";
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Landing from "./pages/Landing.jsx";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" >
+    <Route path="/">
       <Route path="" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
       <Route path="source" element={<Source />} />
       <Route path="latest-news" element={<LatestNews />} />
-      <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
     </Route>
   )
 );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { FaGoogle, FaFacebook } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setError("Please fill in ALL the fields!");
+      console.log(error)
+      return;
+    }
+
+    setError("");
+    setLoading(true)
+
+    try {
+      let API_BASE_URL = "http://localhost:5000";
+
+      const response = await axios.post(`${API_BASE_URL}/users/login`, {
+        email,
+        password,
+      });
+      console.log(response.data);
+
+      if(response.data.message === 'Login Successfull'){
+        localStorage.setItem('token', response.data.token)
+        navigate('/home')
+      }
+    } catch (error) {
+      setError(error)
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
       <div className="bg-white p-8 rounded-lg shadow-xl w-96">
@@ -13,13 +54,16 @@ const Login = () => {
           Login to your News Aggregator account
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
+            {error && <div className="flex justify-center text-red-400">{error.response.data.error}</div>}
             <label className="block text-gray-700">Email</label>
             <input
               type="email"
               placeholder="Enter your email"
+              value={email}
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -28,13 +72,15 @@ const Login = () => {
             <input
               type="password"
               placeholder="Enter your password"
+              value={password}
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
           <div className="flex justify-between items-center mb-4">
             <label className="flex items-center">
-              <input type="checkbox" className="mr-2" />
+              <input type="checkbox" className="mr-2 cursor-pointer" />
               <span className="text-gray-600">Remember Me</span>
             </label>
             <a href="#" className="text-blue-500 text-sm hover:underline">
@@ -44,9 +90,10 @@ const Login = () => {
 
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+            disabled={loading}
+            className={`w-full  text-white py-2 rounded-lg cursor-pointer transition duration-300 ${loading ? "bg-blue-300 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"}`}
           >
-            Login
+            {loading ? "Logging in..." : "Login"}
           </button>
         </form>
 
