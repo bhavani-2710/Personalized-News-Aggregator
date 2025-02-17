3bf6d3e963469ad0ed55ceef67d8ee5c8ba6934c
Leisha Suvarna <leishasuv29@gmail.com>
2025-02-17 14:41:17 +0530

    made changes to login and register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -16,53 +16,43 @@ const Login = () => {
 
     if (!email || !password) {
       setError("Please fill in ALL the fields!");
-      console.log(error)
       return;
     }
 
     setError("");
-    setLoading(true)
+    setLoading(true);
 
     try {
       let API_BASE_URL = "http://localhost:5000";
-
-      const response = await axios.post(`${API_BASE_URL}/users/login`, {
-        email,
-        password,
-      });
-      console.log(response.data);
-
-      if(response.data.message === 'Login Successfull'){
-        localStorage.setItem('token', response.data.token)
-        navigate('/home')
+      const response = await axios.post(`${API_BASE_URL}/users/login`, { email, password });
+      
+      if (response.data.message === "Login Successfull") {
+        localStorage.setItem("token", response.data.token);
+        navigate("/home");
       }
     } catch (error) {
-      setError(error)
-      console.log(error)
+      setError(error.response?.data?.error || "Login Failed");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
-      <div className="bg-white p-8 rounded-lg shadow-xl w-96">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
-          Welcome Back!
-        </h2>
-        <p className="text-center text-gray-600 mb-6">
-          Login to your News Aggregator account
-        </p>
+    <div className="flex items-center justify-center h-screen bg-yellow-950">
+      <div className="bg-white p-10 rounded-lg shadow-2xl w-md">
+        <h2 className="text-5xl font-bold text-yellow-600 text-center mb-6">Welcome Back!</h2>
+        <p className="text-center text-gray-700 mb-6">Login to your NewsHub account</p>
+
+        {error && <div className="text-red-500 text-center mb-4">{error}</div>}
 
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            {error && <div className="flex justify-center text-red-400">{error.response.data.error}</div>}
             <label className="block text-gray-700">Email</label>
             <input
               type="email"
               placeholder="Enter your email"
               value={email}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-yellow-600"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
@@ -73,33 +63,23 @@ const Login = () => {
               type="password"
               placeholder="Enter your password"
               value={password}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-yellow-600"
               onChange={(e) => setPassword(e.target.value)}
             />
-          </div>
-
-          <div className="flex justify-between items-center mb-4">
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2 cursor-pointer" />
-              <span className="text-gray-600">Remember Me</span>
-            </label>
-            <a href="#" className="text-blue-500 text-sm hover:underline">
-              Forgot Password?
-            </a>
           </div>
 
           <button
             type="submit"
             disabled={loading}
-            className={`w-full  text-white py-2 rounded-lg cursor-pointer transition duration-300 ${loading ? "bg-blue-300 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"}`}
+            className={`w-full py-2 rounded-lg text-white text-lg font-bold transition duration-300 ${loading ? "bg-yellow-400 cursor-not-allowed" : "bg-yellow-600 hover:bg-yellow-700"}`}
           >
             {loading ? "Logging in..." : "Login"}
           </button>
         </form>
 
-        <div className="mt-6 text-center text-gray-600">OR</div>
+        <div className="text-center text-gray-700 my-6">OR</div>
 
-        <div className="flex justify-center space-x-4 mt-4">
+        <div className="flex justify-center space-x-4">
           <button className="flex items-center space-x-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300">
             <FaGoogle /> <span>Google</span>
           </button>
@@ -108,9 +88,9 @@ const Login = () => {
           </button>
         </div>
 
-        <p className="text-center text-gray-600 mt-6">
-          Don't have an account?{" "}
-          <Link to="/register" className="text-blue-500 hover:underline">
+        <p className="text-center text-gray-700 mt-6">
+          Don't have an account? {" "}
+          <Link to="/register" className="text-yellow-600 hover:underline">
             Sign up
           </Link>
         </p>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,14 +4,10 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-500 to-blue-600">
-      <div className="bg-white p-8 rounded-lg shadow-xl w-96">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
-          Create an Account
-        </h2>
-        <p className="text-center text-gray-600 mb-6">
-          Join the News Aggregator today!
-        </p>
+    <div className="flex items-center justify-center h-screen bg-yellow-950">
+      <div className="bg-white p-10 rounded-lg shadow-2xl w-md">
+        <h2 className="text-5xl font-bold text-yellow-600 text-center mb-6">Create an Account</h2>
+        <p className="text-center text-gray-700 mb-6">Join the NewsHub today!</p>
 
         <form>
           <div className="mb-4">
@@ -19,7 +15,7 @@ const Register = () => {
             <input
               type="text"
               placeholder="Enter your full name"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-green-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-yellow-600"
             />
           </div>
 
@@ -28,7 +24,7 @@ const Register = () => {
             <input
               type="email"
               placeholder="Enter your email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-green-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-yellow-600"
             />
           </div>
 
@@ -37,32 +33,32 @@ const Register = () => {
             <input
               type="password"
               placeholder="Create a password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-green-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-yellow-600"
             />
           </div>
 
           <button
             type="submit"
-            className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition duration-300"
+            className="w-full py-2 rounded-lg text-white text-lg font-bold transition duration-300 bg-yellow-600 hover:bg-yellow-700"
           >
             Register
           </button>
         </form>
 
-        <div className="mt-6 text-center text-gray-600">OR</div>
+        <div className="text-center text-gray-700 my-6">OR</div>
 
-        <div className="flex justify-center space-x-4 mt-4">
+        <div className="flex justify-center space-x-4">
           <button className="flex items-center space-x-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300">
-            <FaGoogle /> <span>Sign up with Google</span>
+            <FaGoogle /> <span>Google</span>
           </button>
           <button className="flex items-center space-x-2 bg-blue-800 text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition duration-300">
-            <FaFacebook /> <span>Sign up with Facebook</span>
+            <FaFacebook /> <span>Facebook</span>
           </button>
         </div>
 
-        <p className="text-center text-gray-600 mt-6">
-          Already have an account?{" "}
-          <Link to="/login" className="text-green-500 hover:underline">
+        <p className="text-center text-gray-700 mt-6">
+          Already have an account? {" "}
+          <Link to="/login" className="text-yellow-600 hover:underline">
             Login
           </Link>
         </p>
