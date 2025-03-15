147ba85aa3957da50560d39e7f2240da238ac38f
Bhavani Murali <bhavani9271@gmail.com>
2025-03-15 12:47:46 +0530

    removed jwt authentication

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const newsRouter = require("./routes/newsRoutes");
 const authRoutes = require("./routes/authRoutes");
 const historyRoutes = require("./routes/historyRoutes");
 const {
-  default: jwtAuthMiddleware,
+  jwtAuthMiddleware,
 } = require("./middlewares/jwtAuthMiddleware");
 
 const app = express();
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 connectDB();
 
 // Routes
-app.use("/news", jwtAuthMiddleware, newsRouter);
+app.use("/news", newsRouter);
 app.use("/users", authRoutes);
 app.use("/history", historyRoutes);
 
@@ -29,5 +29,5 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
